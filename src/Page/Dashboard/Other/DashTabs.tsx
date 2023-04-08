1f0b4db559f7119afebca96ff47e9b4d1ed06d46
unknown <javohirbozorboyev2002@gmail.com>
2023-04-08 21:07:56 +0500

    tabs activlik hatosi bartaraf etildi

--- src/Page/Dashboard/Other/DashTabs.tsx
+++ src/Page/Dashboard/Other/DashTabs.tsx
@@ -1,16 +1,13 @@
-/* eslint-disable react/jsx-key */
-import { ScrollArea, Tabs, useMantineTheme } from "@mantine/core";
+import { ActionIcon, Tabs, useMantineTheme } from "@mantine/core";
 import { useRouter } from "next/router";
-import React, { useMemo, useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
-  TbCalendarStats,
   TbDashboard,
+  TbHome,
   TbLayoutDashboard,
-  TbPhoto,
+  TbMessageCircle,
   TbShoppingBag,
 } from "react-icons/tb";
-
-type Props = {};
 
 const Links = [
   {
@@ -23,57 +20,39 @@ const Links = [
     icon: TbShoppingBag,
     url: "/dashboard/course",
   },
-  //   {
-  //     label: "Releases",
-  //     icon: TbCalendarStats,
-  //     initiallyOpened: true,
-  //     links: [
-  //       { label: "Upcoming releases", link: "/a" },
-  //       { label: "Previous releases", link: "/b" },
-  //       { label: "Releases schedule", link: "/c" },
-  //     ],
-  //   },
 ];
+
+type Props = {};
 
 const DashTabs = (props: Props) => {
   const router = useRouter();
-  const [activTab, setActiveTab] = useState(router.route || "");
   const theme = useMantineTheme();
-  const LinksMemo = useMemo(() => {
-    return Links;
-  }, []);
-
-  useEffect(() => {
-    setActiveTab(router.route);
-    console.log("sa");
-  }, [router.route]);
   return (
     <div>
       <Tabs
-        defaultValue={activTab}
-        color="yellow"
+        value={router.route as string}
+        onTabChange={(value) => router.push(`${value}`)}
         m={"-md"}
         mb={"lg"}
-        radius={"0"}
+        radius={0}
+        color="yellow"
         sx={(theme) => ({
           backgroundColor:
             theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+          borderRadius: "4px",
         })}
-        value={router.query.activeTab as string}
-        onTabChange={(value) => router.push(`${value}`)}
       >
         <Tabs.List grow>
-          {LinksMemo.map((link: any, i: number) => {
+          {Links.map((item, i) => {
             return (
               <Tabs.Tab
                 key={i}
-                p={"md"}
-                value={link.url}
+                p={"sm"}
                 icon={
-                  <link.icon
-                    size={"1.2rem"}
+                  <item.icon
+                    size="1.5rem"
                     color={`${
-                      router.route === link.url
+                      router.route === item.url
                         ? theme.colorScheme === "dark"
                           ? theme.colors.yellow[2]
                           : theme.colors.yellow[5]
@@ -83,7 +62,8 @@ const DashTabs = (props: Props) => {
                     }`}
                   />
                 }
-              ></Tabs.Tab>
+                value={item.url}
+               />
             );
           })}
         </Tabs.List>
