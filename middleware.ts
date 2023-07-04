8dd7c9a5bc2c054fc98ab4cef112d88b767365ff
Acer <javohirbozorboyev2002@gmail.com>
2023-07-04 21:01:33 +0500

    page sign in

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,18 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {},
+  function middleware(req) {
+    console.log(req.nextauth);
+  },
 
   {
     callbacks: {
       authorized: ({ token }) => !!token,
     },
     secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+      signIn: "/login/signin",
+    },
   }
 );
 
