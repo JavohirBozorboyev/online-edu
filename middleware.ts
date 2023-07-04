642fb6cc2c9c39247d25a3e8e5ca9f93e7561e9c
Acer <javohirbozorboyev2002@gmail.com>
2023-07-04 21:07:01 +0500

    error ni bild qilsih

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,7 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
-    console.log(req.nextauth);
-  },
+  function middleware(req) {},
 
   {
     callbacks: {
